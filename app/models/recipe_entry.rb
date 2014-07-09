f766d92dbe71ad781a5de6d48e3e3f759e31fa79
zhbanton <zhbanton@gmail.com>
2014-07-09 03:44:47 -0400

    Assignment of ingredients to user fixed

--- app/models/recipe_entry.rb
+++ app/models/recipe_entry.rb
@@ -4,6 +4,8 @@ class RecipeEntry < ActiveRecord::Base
 
   accepts_nested_attributes_for :ingredient, reject_if: :all_blank
 
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+
   def ingredient_attributes=(ingredient_attributes)
     ingredient = Ingredient.find_or_create_by(name: ingredient_attributes[:name].downcase, user_id: ingredient_attributes[:user_id])
     self.ingredient_id = ingredient.id
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :recipes
-  has_many :ingredients, through: :recipes
+  has_many :ingredients
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
